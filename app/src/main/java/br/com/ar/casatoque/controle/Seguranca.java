74d6d09ba5df0885b157bb92aff418211ed3d45d
grossferreira <grossferreira@gmail.com>
2015-03-16 22:21:26 -0300

    Initial commit

--- app/src/main/java/br/com/ar/casatoque/controle/Seguranca.java
+++ app/src/main/java/br/com/ar/casatoque/controle/Seguranca.java
@@ -4,12 +4,16 @@ package br.com.ar.casatoque.controle;
 import br.com.ar.casatoque.R;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class Seguranca extends Fragment {
+
+public class Seguranca extends Fragment implements OnClickListener {
 	
 	public Seguranca(){}
 	
@@ -18,7 +22,40 @@ public class Seguranca extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.seguranca, container, false);
-         
+
+        //chamada da tela de alame da residencia
+        Button btnAlarme = (Button) rootView.findViewById(R.id.btAlarme);
+        btnAlarme.setOnClickListener(this);
+
+        //chamada da tela de alame da residencia
+        Button btnEventos = (Button) rootView.findViewById(R.id.btEventos);
+        btnEventos.setOnClickListener(this);
+
         return rootView;
     }
-}
+
+    /**
+     * Inicia determinada Activity dependendo do botao clicado
+     */
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        int requestCode = 0;
+
+        switch (v.getId()) {
+
+            case R.id.btAlarme:
+                i = new Intent(getActivity(), Alarme.class);
+            break;
+
+            case R.id.btEventos:
+                i = new Intent(getActivity(), Eventos.class);
+            break;
+
+        }
+
+        if(i != null){
+            startActivityForResult(i, requestCode);
+        }
+    }
+}//fim

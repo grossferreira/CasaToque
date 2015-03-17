58025d25c0afee719f04164d100ef90241da16fa
grossferreira <grossferreira@gmail.com>
2015-03-16 22:22:54 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ar/casatoque/controle/Eventos.java
@@ -0,0 +1,27 @@
+package br.com.ar.casatoque.controle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.com.ar.casatoque.R;
+
+
+    public class Eventos extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eventos);
+
+    }
+
+}
